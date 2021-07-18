fda310f14d93df3c26bde87974657fd37883f055
Onur Karal <santobixua@gmail.com>
2021-07-18 16:35:18 +0300

    Methods

---
+++ CsharpIntermediate/Point.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CsharpIntermediate
+{
+    public class Point
+    {
+        public int X;
+        public int Y;
+
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+
+        public void Move(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+
+        }
+
+        public void Move(Point newLocation)
+        {
+
+            if (newLocation == null)
+                throw new ArgumentException("new location");
+            Move(newLocation.X,newLocation.Y);
+        }
+    }
+}
--- CsharpIntermediate/Program.cs
+++ CsharpIntermediate/Program.cs
@@ -8,13 +8,44 @@ namespace CsharpIntermediate
         static void Main(string[] args)
         {
 
-            var customer = new Customer(3,"Onur");
-            // var order = new Order();
-            // customer.Orders.Add(order);
+      
+       
 
-            Console.WriteLine(customer.Id);
+
+        }
+
+        static void UseCustomer()
+        {
+            var customer = new Customer(3, "Onur");
+            var order = new Order();
+            customer.Orders.Add(order);
+            Console.WriteLine(customer.Id); 
             Console.WriteLine(customer.Name);
 
+        }
+
+        static void UseParams()
+        {
+            var calculator = new Calculator();
+            Console.WriteLine(calculator.Add(1, 2));
+            Console.WriteLine(calculator.Add(1, 2, 3, 3));
+        }
+        static void UsePoints()
+        {
+            try
+            {
+                var point = new Point(10, 20);
+                point.Move(null);
+                Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+
+                point.Move(100, 200);
+                Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An unexpected  error");
+
+            }
         }
     }
 }
